11e9ea1d5abaed10c5d1b4cfcf9e5534465c572f
Button99 <56029580+Button99@users.noreply.github.com>
2022-06-16 19:06:35 +0300

    Temperature - Humidity Sensor with Blynk.cloud

---
+++ TEMP_HUM_SENS/TEMP_HUM_SENS.ino
@@ -0,0 +1,54 @@
+#define BLYNK_PRINT Serial
+
+#define BLYNK_TEMPLATE_ID "TMPL6yX7wjdV"
+#define BLYNK_DEVICE_NAME "TEMP HUM"
+#define BLYNK_AUTH_TOKEN "6GoU4dMnpLDp3elflmqCGRQvksZr-04v"
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+#include "DHT.h"
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "XXX";
+char pass[] = "XXX";
+
+
+#define DHTTYPE DHT11
+#define DHTPIN 25
+
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V0, t);
+  Blynk.virtualWrite(V1, h);
+}
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+
+  dht.begin();
+  timer.setInterval(1000L, sendSensor);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+}
